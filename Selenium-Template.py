b707a1f05a4296f5c0ee655c1b6ecaaf1711fc18
manjot3242 <123349291+manjot3242@users.noreply.github.com>
2023-01-23 11:14:00 +0530

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
@@ -22,8 +23,8 @@ options = [
     #"--window-size=1920,1200",
     #"--ignore-certificate-errors",
     #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
     #'--remote-debugging-port=9222'
 ]
 
@@ -33,8 +34,7 @@ for option in options:
     
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
-
+driver.get('https://mytoolstown.com/smsbomber/')
+driver = get_drvier()
+driver.find_element(by= "id" ,value="mobno").send_keys("8872213990")
+driver.find_element(by="id", value="count").send_keys("1" + Keys.RETURN)
